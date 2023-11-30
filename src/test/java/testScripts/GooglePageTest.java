84e8c2b1fd1028381edb56c6b3adbc62b2806bd6
swpooja <pooja.malve1998@gmail.com>
2023-11-30 06:46:33 +0000

    Initial project with webDriverSamples

---
+++ src/test/java/testScripts/GooglePageTest.java
@@ -0,0 +1,35 @@
+package testScripts;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class GooglePageTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.navigate().to("https://www.google.com/");
+		System.out.println("Main title.."+driver.getTitle());
+		WebElement srcBox = driver.findElement(By.id("APjFqb"));
+		srcBox.sendKeys("Java Tutorial");
+		srcBox.sendKeys(Keys.ENTER);
+		System.out.println("URL.."+ driver.getCurrentUrl());
+		driver.navigate().back();
+		System.out.println("Main title.."+driver.getTitle());
+		driver.navigate().forward();
+		System.out.println("current title.."+driver.getTitle());
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
